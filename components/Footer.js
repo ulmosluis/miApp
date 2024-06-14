70fdf1008903d3ff6e5e53226a074f774c7e431c
unknown <ulmosluis@gmail.com>
2024-06-14 05:30:06 -0600

    con componentes! -empezando solo ensenando, no corre

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return <h2>Hi, I am a Footer!</h2>;
+  }
+}
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,11 @@
+
+
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return <h2>Hi, I am a Header!</h2>;
+  }
+}
+
+export default Header;
