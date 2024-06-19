7732a00cd9dd7e7814ca25c03be25974128c63a5
unknown <ulmosluis@gmail.com>
2024-06-19 06:17:59 -0600

    volvi al sin conponentes voy a hacerlo paso a paso otra vez

---
+++ src/App3.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Home from "./Home";
+import Services from "./Services";
+import About from "./About";
+import Navbar from "./components/Header";
+import Footer from "./components/Footer";
+import Whatsapp from "./components/whatsapp"; 
+const LandingPage = () => {
+  return (
+    <div>
+     <Navbar />
+      <Whatsapp /> 
+      <Home />
+      <About />
+      <Services />
+      <Footer /> 
+    </div>
+  )
+}
